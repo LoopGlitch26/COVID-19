ff8cbb8aec46c7789c719604ad2f1a3e94c9983a
Bravish Ghosh <53336715+LoopGlitch26@users.noreply.github.com>
2020-04-10 11:33:04 +0530

    Add files via upload
    Live updates about COVID-19 active cases, deaths, and recoveries in India

---
+++ India Covid-19 update.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+print("Welcome to Covid-19 live updates")
+website="https://www.worldometers.info/coronavirus/country/india/"
+a=requests.get(website)
+b=BeautifulSoup(a.text, "html.parser")
+data_abs=b.find_all("div", class_="maincounter-number")
+print("Total Cases = ", data_abs[0].text.strip())
+print("Total Deaths = ", data_abs[1].text.strip())
+print("Total Recovered = ", data_abs[2].text.strip())
